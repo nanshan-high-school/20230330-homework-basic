c1eecd2a2efd7e983d743b073cd85e5ea325639e
homework <homework970708@gmail.com>
2023-04-12 21:40:06 +0800

    955022 33922莊孟儒 5*5 bingo

--- 955022/bingo.cpp
+++ 955022/bingo.cpp
@@ -1,56 +1,92 @@
-#include <cstdlib>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
-  int count = 0;
-  int bingo[5][5];
-  int number[25] = {0};
-  srand(time(0));
-  for (int i = 0; i < 25; i++) {
-    int pick;
-    do {
-      pick = rand() % 25 + 1;
-    } while (number[pick - 1] > 0);
+    srand(time(NULL));
+    int count = 1, n = 0, line = 0, num[25], array[5][5] = {0};
+    bool finish=true;
 
-    number[pick - 1]++;
-
-    cout << pick << "  ";
-    count++;
-    if (count == 5) {
-      cout << '\n';
-      count = 0;
+    for (int i = 0; i < 25; i++) {
+        num[i] = i + 1;
     }
-  }
 
-  for (int i = 0; i < 25; i++) {
-    int y = i / 5;
-    int x = i % 5;
-    bingo[y][x] = number[i];
-  }
-
-  // for (int i = 0; i < 5; i++) {
-  //     for (int j = 0; j < 5; j++) {
-  //         bingo[i][j] = number[5*i+j];
-  //     } // j
-  // } // i
-
-  int number2[25] = {0};
-  int question[25] = {0};
-  for (int i = 0; i < 25; i++) {
-    int pick;
-    do {
-      pick = rand() % 25 + 1;
-    } while (number[pick] > 0);
-
-    number[pick - 1]++;
-
-    question[i] = pick + 1;
-
-    // bingo[0][0~2]
-    for (int j = 0; j <= i; j++) {
-      if (bingo[0][0] == question[i]) {
-      }
+    for (int i = 0; i < 25; i++) {
+        int j = rand() % 25;
+        int temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
     }
-  }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            array[i][j] = num[count - 1];
+            cout << array[i][j] << " ";
+            count++;
+        }
+        cout << endl;
+    }
+
+    while(finish==true){
+        n=rand()%25+1;
+        array[n / 5][n%5]=0;
+        cout << "{" << n / 5 << "," << n % 5 << "}"<< endl;
+
+        for(int i=0;i<5;i++){
+            if(array[0][i]==0){
+                line=0;
+                for(int j=0;j<5;j++){
+                    if(array[i][j]==0){
+                        line++;
+                    }
+                } 
+                if (line==5){
+                    cout<<"bingo!";
+                    return(0);
+                }
+            }
+        }
+
+        for(int i=0;i<5;i++){
+            if(array[i][0]==0){
+                line=0;
+                    for(int j=0;j<5;j++){
+                        if(array[j][i]==0){
+                            line++;
+                        }
+                    }
+                    if (line==5){
+                        cout<<"bingo!";
+                        return(0);
+                    }
+            }
+        }
+
+        if(array[0][0]==0){
+            line=0;
+                for(int j=0;j<5;j++){
+                    if(array[j][j]==0){
+                        line++;
+                    }
+                }
+                if (line==5){
+                    cout<<"bingo!";
+                    return(0);
+                }
+        }
+
+        if(array[4][0]==0){
+            line=0;
+                for(int j=0;j<5;j++){
+                    if(array[4-j][j]==0){
+                        line++;
+                    }
+                }
+                if (line==5){
+                    cout<<"bingo!";
+                    return(0);
+                }
+        }
+    }
 }
